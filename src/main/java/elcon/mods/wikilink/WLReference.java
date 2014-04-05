f35bbf1ad2def4f392641aaf5aecbf68b02509f5
ElConquistador <itsme.elconquistador@gmail.com>
2014-04-05 14:07:16 +0200

    Updated build script

--- src/main/java/elcon/mods/wikilink/WLReference.java
+++ src/main/java/elcon/mods/wikilink/WLReference.java
@@ -4,7 +4,7 @@ public class WLReference {
 
 	public static final String MOD_ID = "WikiLink";
 	public static final String NAME = "WikiLink";
-	public static final String VERSION = "${version} (build ${buildnumber})";
+	public static final String VERSION = "1.0.0";
 	public static final String MC_VERSION = "[1.7.2]";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.12.0.1024,)";
 	public static final String SERVER_PROXY_CLASS = "elcon.mods.wikilink.WLCommonProxy";
